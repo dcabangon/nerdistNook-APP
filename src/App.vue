d197037eba100b7959bcb897702c5dc0888d3cd9
dcabangon <richardnelsoncabangon@gmail.com>
2022-01-25 15:59:10 +0000

    added initial navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <!-- Links -->
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <button class="nav-link active" aria-current="page">Recipes</button>
+      </li>
+      <li class="nav-item">
+        <button class="nav-link" href="#">Add new</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
