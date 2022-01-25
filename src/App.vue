4e1c6046092bd27f07f51bd3972ddd6f11372cfd
dcabangon <richardnelsoncabangon@gmail.com>
2022-01-25 17:16:22 +0000

    programmed 2 pages

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,46 @@
     <!-- NAVBAR -->
     <ul class="nav nav-tabs">
       <li class="nav-item">
-        <button class="nav-link active" aria-current="page">Recipes</button>
+        <button v-on:click="goRecipes" class="nav-link active" aria-current="page">
+          Recipes
+        </button>
       </li>
       <li class="nav-item">
-        <button class="nav-link">Add new</button>
+        <button v-on:click="goAddRecipe" class="nav-link">Add new</button>
       </li>
     </ul>
     <!-- END OF NAVBAR -->
     <div>
-      <Recipes/>
-      <AddRecipe/>
+      <Recipes v-if="page ==='recipes'" />
+      <AddRecipe v-if="page ==='add'" />
     </div>
   </div>
 </template>
 
 <script>
-import AddRecipe from "@/components/AddRecipe"
+import AddRecipe from "@/components/AddRecipe";
 import Recipes from "@/components/Recipes";
 
 export default {
   name: "App",
   components: {
-    Recipes, AddRecipe
+    Recipes,
+    AddRecipe,
+  },
+
+  data: function () {
+    return {
+      page: "recipes",
+    };
+  },
+
+  methods: {
+    "goRecipes": function () {
+      this.page = "recipes";
+    },
+    "goAddRecipe": function () {
+      this.page = "add";
+    },
   },
 };
 </script>
