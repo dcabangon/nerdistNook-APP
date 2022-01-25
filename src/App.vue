27b46f736a5e6bc4e656f63a865e0446bfeeddcf
dcabangon <richardnelsoncabangon@gmail.com>
2022-01-25 16:36:12 +0000

    added page components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="container-fluid">
-    <!-- Links -->
+    <!-- NAVBAR -->
     <ul class="nav nav-tabs">
       <li class="nav-item">
         <button class="nav-link active" aria-current="page">Recipes</button>
       </li>
       <li class="nav-item">
-        <button class="nav-link" href="#">Add new</button>
+        <button class="nav-link">Add new</button>
       </li>
     </ul>
+    <!-- END OF NAVBAR -->
+    <div>
+      <Recipes/>
+      <AddRecipe/>
+    </div>
   </div>
 </template>
 
 <script>
- 
+import AddRecipe from "@/components/AddRecipe"
+import Recipes from "@/components/Recipes";
 
 export default {
-  name: 'App',
-
-}
+  name: "App",
+  components: {
+    Recipes, AddRecipe
+  },
+};
 </script>
 
 <style>
-
 </style>
