d0ab72cc71492a8bf6118e7d46f740d975e15eec
dcabangon <richardnelsoncabangon@gmail.com>
2022-01-26 11:52:28 +0000

    Added script on component

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,3 +1,16 @@
 <template> 
 <h1>All Recipe</h1>
 </template> 
+
+<script>
+const BASE_API_URL="https://3000-dcabangon-nerdistnookapi-xwxhxz2ave4.ws-us29.gitpod.io/"
+
+import axios from 'axios'
+export default{
+    created: async function() {
+        let response = await axios.get(BASE_API_URL +'/item_entry')
+        console.log(response.data)
+    }
+}
+</script>
+
