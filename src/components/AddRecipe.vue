2dfaf0745a112fb169888ff42e8f76ee83421e1e
dcabangon <richardnelsoncabangon@gmail.com>
2022-01-26 14:20:42 +0000

    programmed 2 components

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,3 +1,32 @@
-<template> 
-<h1>Add Recipe</h1>
+<template>
+  <div>
+    <h1>Add Recipe</h1>
+
+    <div>
+      <label>Book Name</label>
+      <input type="text" v-model="bookName" class="form-control"/>
+    </div>
+
+    <div>
+      <label>Publisher</label>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Seperate each ingredient with a comma"
+        v-model="publisher"/>
+    </div>
+    <button v-on:click="addNew">Add New</button>
+  </div>
 </template> 
+
+
+<script>
+export default {
+  data: function () {
+    return {
+      bookName: "",
+      publisher: "",
+    };
+  },
+};
+</script>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,16 +1,38 @@
-<template> 
-<h1>All Recipe</h1>
+<template>
+  <div>
+    <h1>All Recipe</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="r in recipes" v-bind:key="r._id">
+        <td>{{ r.bookName }}</td>
+        <td>{{ r.publisher }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template> 
 
 <script>
-const BASE_API_URL="https://3000-dcabangon-nerdistnookapi-xwxhxz2ave4.ws-us29.gitpod.io/"
+const BASE_API_URL =
+  "https://3000-dcabangon-nerdistnookapi-xwxhxz2ave4.ws-us29.gitpod.io/";
 
-import axios from 'axios'
-export default{
-    created: async function() {
-        let response = await axios.get(BASE_API_URL +'/item_entry')
-        console.log(response.data)
-    }
-}
+import axios from "axios";
+export default {
+  created: async function () {
+    let response = await axios.get(BASE_API_URL + "item_entry");
+    this.recipes = response.data;
+  },
+  data: function () {
+    return {
+      recipes: [],
+    };
+  },
+};
 </script>
 
