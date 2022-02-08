152a32cdbe69f1d567fa193cc9564a3e57eaa2b7
dcabangon <richardnelsoncabangon@gmail.com>
2022-02-08 14:32:18 +0000

    about us update

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,19 +1,73 @@
 <template>
   <div class="aboutus-div">
     <h1 class="my-3">About Us</h1>
- 
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget massa
+      risus. Mauris tincidunt libero eu sem imperdiet, id elementum enim
+      blandit. Quisque dapibus neque non ligula tempus, in placerat risus
+      tincidunt. Sed a leo vulputate, ultricies nibh vitae, porttitor nisl.
+      Pellentesque tempor accumsan nulla, sit amet mattis diam pellentesque
+      eget. Donec sed justo felis. Morbi sed velit turpis. Maecenas congue
+      tristique nisi eget semper. Fusce commodo venenatis interdum.
+    </p>
+    <br>
+    <p>
+      Sed tincidunt a augue non posuere. Sed laoreet justo ut fringilla
+      sollicitudin. Aenean ullamcorper tellus eget nibh convallis elementum.
+      Curabitur molestie justo sit amet nibh consectetur interdum. Fusce sit
+      amet tellus sed lorem auctor auctor. Aliquam erat volutpat. Phasellus
+      finibus mollis eleifend. Vestibulum at laoreet dolor. Nulla mollis dapibus
+      justo id ullamcorper. Vestibulum sed feugiat eros, at egestas eros.
+      Vivamus lobortis ac risus sit amet lacinia.
+    </p>
+    <br>
+    <p>
+      Pellentesque vitae massa metus. Nulla in velit id ante molestie lobortis.
+      Proin porta vel urna eget maximus. Donec vel sapien non risus mattis
+      eleifend. Proin tempus nunc at luctus mattis. Integer a dapibus purus.
+      Integer porta et enim eget volutpat. Maecenas tristique nulla et molestie
+      sagittis.
+    </p>
+    <br>
+    <p>
+      Aliquam ultricies lorem nec bibendum commodo. Phasellus et luctus turpis.
+      Etiam pulvinar felis dolor, et aliquam ante semper eu. Pellentesque
+      aliquet odio dapibus aliquam sodales. Phasellus malesuada, mi sit amet
+      fermentum consequat, mi libero consectetur nunc, nec elementum neque
+      tortor nec risus. Nulla facilisi. Orci varius natoque penatibus et magnis
+      dis parturient montes, nascetur ridiculus mus. Donec finibus faucibus
+      neque, id hendrerit nisi dictum nec. Curabitur semper convallis tincidunt.
+      Nunc pulvinar eros nec dapibus varius. Nunc sollicitudin elementum
+      vestibulum. Phasellus eget sapien vel sem imperdiet interdum. Nunc ac
+      hendrerit leo. Nam ut vestibulum quam. Ut ut sapien lobortis, consequat
+      tellus interdum, facilisis tellus.
+    </p>
+    <br>
+    <p>
+      Nam suscipit facilisis sem quis iaculis. Aenean porttitor mi id justo
+      lobortis rutrum. Mauris consectetur mauris porttitor urna pulvinar, nec
+      maximus nisi pharetra. Morbi laoreet faucibus sollicitudin. Suspendisse
+      convallis consequat odio a gravida. Maecenas eu sapien metus. Praesent
+      mollis risus rhoncus urna porta interdum. Vivamus commodo, ex nec mattis
+      sodales, dui urna lobortis risus, id tempus felis nisi in mi. Donec ut
+      tincidunt diam, a mollis sem. Curabitur ultrices justo vel risus tempor,
+      ut pretium tellus volutpat. Vestibulum non tincidunt augue, quis efficitur
+      libero. In ut leo est. Donec auctor dignissim sapien, quis suscipit dolor
+      varius ac. Vivamus eleifend sem sit amet tempus pretium. Donec scelerisque
+      lacus a turpis elementum fringilla.
+    </p>
   </div>
 </template>
 
 <style>
-/* .aboutus-div{
+.aboutus-div{
   height: 100vh;
   background: white;
   padding: 5px;
   padding-left: 4px;
   padding-right: 4px;
   opacity: 70%;
-} */
+}
 
 
 </style>
