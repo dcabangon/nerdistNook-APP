4291a741726cc54065a46d9b56315ee6d8ddf826
dcabangon <richardnelsoncabangon@gmail.com>
2022-02-08 11:17:33 +0000

    updated edit book form

--- src/components/EditBook.vue
+++ src/components/EditBook.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="margin-form">
-    <h1>Edit Book</h1>
-    <div>
-      <label>Book Name</label>
-      <input type="text" v-model="bookName" class="form-control" />
-    </div>
+  <div class="editbook-div">
+    <div class="margin-form">
+      <h1>Edit Book</h1>
+      <div>
+        <label>Book Name</label>
+        <input type="text" v-model="bookName" class="form-control" />
+      </div>
 
-    <div>
-      <label>Issue</label>
-      <input type="text" v-model="issueNumber" class="form-control" />
-    </div>
-  
-    <div>
-      <label>Publisher</label>
-      <input type="text" v-model="publisher" class="form-control" />
-    </div>
-    
-    <div>
-      <label>Review</label>
-      <input type="text" v-model="review" class="form-control" />
-    </div>
-   
-    <div>
-      <label>Image Link</label>
-      <input type="text" v-model="imageLink" class="form-control" />
-    </div>
-   
-    <div>
-      <label>Rating</label>
-      <b-form-rating
-        v-model="rating"
-        variant="warning"
-        class="mb-2"
-      ></b-form-rating>
-    </div>
+      <div>
+        <label>Issue</label>
+        <input type="text" v-model="issueNumber" class="form-control" />
+      </div>
 
-    <button v-on:click="updateBook" class="my-3">Update</button>
+      <div>
+        <label>Publisher</label>
+        <input type="text" v-model="publisher" class="form-control" />
+      </div>
+
+      <div>
+        <label>Review</label>
+        <input type="text" v-model="review" class="form-control" />
+      </div>
+
+      <div>
+        <label>Image Link</label>
+        <input type="text" v-model="imageLink" class="form-control" />
+      </div>
+
+      <div>
+        <label>Rating</label>
+        <b-form-rating
+          v-model="rating"
+          variant="warning"
+          class="mb-2"
+        ></b-form-rating>
+      </div>
+
+      <button v-on:click="updateBook" class="my-3">Update</button>
+    </div>
   </div>
 </template>
 
@@ -87,9 +89,22 @@ export default {
 </script>
 
 <style scoped>
+.editbook-div {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
 .margin-form {
-  margin-top: 50px;
+  /* margin-top: 50px;
   margin-left: 300px;
-  margin-right: 300px;
+  margin-right: 300px; */
+  background: white;
+  padding: 9px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  width: 100%;
 }
 </style>
